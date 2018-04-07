2aa0e9d68dfe648169f41fd38c0856937dcfb0ac
Mo Ali <hello@moali.me>
2018-04-07 19:12:05 +0200

    General enhancements.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className='cornerSquare bdT d-n@sm-'>
+  <footer className="cornerSquare bdT d-n@sm-">
     <div className="peers jc-c fsz-sm ffm-serif w-60p@xl+ w-90p@xl- fxw-nw">
-      <p className='mR-30 c-critical fw-700 whs-nw mB-0'>My Favorite Quote</p>
-      <blockquote className='c-major mB-0'>“Programs are like women, No matter how much you get mad because of errors..., you are always the one who is wrong”
+      <p className="mR-30 c-critical fw-700 whs-nw mB-0">My Favorite Quote</p>
+      <blockquote className="c-major mB-0">
+        “First, solve the problem. Then, write the code.”
       </blockquote>
     </div>
   </footer>
--- src/shared/scripts/config.js
+++ src/shared/scripts/config.js
@@ -3,8 +3,8 @@ const links = {
   linkedin: 'https://linkedin.com/in/moaali',
   skype: 'skype:mohamed_5691?chat',
   email: 'hello@moali.me',
-  cv: 'https://www.dropbox.com/s/5dzvk597dgtcz6b/Resume.pdf?dl=0',
-  fork: 'https://github.com/moaali/personal-website'
+  cv: 'https://www.dropbox.com/s/hnvuf5e9e54z221/Resume.pdf?dl=0',
+  fork: 'https://github.com/moaali/personal-website',
 };
 
 export default links;
